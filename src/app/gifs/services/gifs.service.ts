c1bcc182470b7abda9bba44c530772ba614b4b85
MLizano <lizanocr@hotmail.com>
2023-10-22 23:55:33 -0600

    first commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,73 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
+
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+
+    public gifList: Gif[] = [];
+    private _tagsHistory: string[] = [];
+    private apiKey: string = 'NNSg7K7SOaO6ffbZB6Px44GWRctw8C33';
+    private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+    constructor(private http: HttpClient) {
+        this.loadLocalSotrage();
+    };
+
+    get tagsHistory() {
+        return [...this._tagsHistory];
+    }
+
+
+    //--- organiza la lista temporal de las busquedas ejecutadas ---
+    private organizeHistory(tag: string) {
+        tag = tag.toLowerCase();
+
+        if (this._tagsHistory.includes(tag)) {
+            this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+        }
+
+        this._tagsHistory.unshift(tag);
+        this._tagsHistory = this._tagsHistory.splice(0, 10);
+        this.saveLocalStorage();
+    }
+
+
+    // almacena en el local storage el historial
+    private saveLocalStorage(): void {
+        localStorage.setItem('history', JSON.stringify(this.tagsHistory));
+    }
+
+
+    // carga el arreglo desde el local storage
+    private loadLocalSotrage(): void {
+        if (!localStorage.getItem('history')) return;
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+        if( this._tagsHistory.length === 0) return;
+        this.searchTag(this.tagsHistory[0]);
+
+    }
+
+
+    //--- llama al API de los gifs y limita la respuesta a 10 gifs ---
+    public searchTag(tag: string): void {
+
+        if (tag.length === 0) return;
+        this.organizeHistory(tag);
+
+        const params = new HttpParams()
+            .set('api_key', this.apiKey)
+            .set('limit', '20')
+            .set('q', tag)
+
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+            .subscribe(resp => {
+                this.gifList = resp.data;
+
+            });
+    }
+
+}
