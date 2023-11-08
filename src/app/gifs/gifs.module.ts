46c9d46e74b376e594e9c07817fb85bce6364354
mlizanoQ6 <mlizano@q6cyber.com>
2023-11-08 15:12:55 -0600

    new_changes

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,28 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardComponent } from './components/card/card.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
-import { CardComponent } from './components/card/card.component';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    HomePageComponent,
-    SearBoxComponent,
-    CardListComponent,
-    CardComponent,
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HomePageComponent,
-  ]
+    declarations: [
+        CardComponent,
+        CardListComponent,
+        HomePageComponent,
+        SearBoxComponent,
+    ],
+    imports: [CommonModule, SharedModule],
+    exports: [HomePageComponent],
 })
-
-
-export class GifsModule { }
+export class GifsModule {}
--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-lazy-image',
-  templateUrl: './lazy-image.component.html',
-  styleUrls: ['./lazy-image.component.css']
+    selector: 'shared-lazy-image',
+    templateUrl: './lazy-image.component.html',
 })
 export class LazyImageComponent {
+    ngOnInit(): void {
+        if (!this.url) {
+            throw new Error('Attribute URL is required');
+        }
+    }
 
+    @Input()
+    public url!: string;
+
+    @Input()
+    public alt: string = 'Image';
+
+    public hasLoaded: boolean = false;
+
+    public onLoad() {
+        this.hasLoaded = true;
+    }
 }
